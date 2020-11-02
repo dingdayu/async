b4284f05b4c91fda99c0d3c2b4e978cb8c1b710c
dingdayu <614422099@qq.com>
2020-11-02 16:30:00 +0800

    add example
    Signed-off-by: dingdayu <614422099@qq.com>

---
+++ example/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/dingdayu/async"
+)
+
+type ExampleAsync struct {
+}
+
+// OnPreRun: Before run, panic panic causes registration failure
+func (a ExampleAsync) OnPreRun() {
+	fmt.Printf("\u001B[1;30;42m[info]\u001B[0m ExampleAsync 注册成功，开始运行！\n")
+}
+
+func (a ExampleAsync) Name() string {
+	return "example"
+}
+
+func (a ExampleAsync) Handle(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		default:
+			// todo:: Logical unit
+			time.Sleep(3 * time.Second)
+			fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (a ExampleAsync) OnShutdown(s os.Signal) {
+	fmt.Printf("\u001B[1;30;42m[info]\u001B[0m ExampleAsync 接收到系统信号[%s]，准备退出！\n", s.String())
+}
+
+func main() {
+	// Handle SIGINT and SIGTERM.
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+
+	ayc := async.NewAsync(context.Background(), ch)
+
+	_ = ayc.Register(ExampleAsync{})
+
+	ayc.Wait()
+	fmt.Println("[1;30;42m[info]\u001B[0m Task exited")
+}
